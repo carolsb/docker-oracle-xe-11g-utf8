2373fb85a519097ff65010000d0afe09c9851512
William Melchior Jablonski <william.jablonski@gmail.com>
2017-10-31 17:57:47 -0200

    Create change_encode.sh

---
+++ assets/change_encode.sh
@@ -0,0 +1,24 @@
+# 1 - Connect
+ssh root@localhost -p 49160
+password: admin
+
+# 2 Change user
+su -l oracle
+
+# 3 conect db
+sqlplus "/as sysdba"
+
+# 4 run sqls
+ shutdown immediate;
+
+ startup restrict;
+
+ select name from v$database;
+
+ ALTER DATABASE CHARACTER SET INTERNAL_USE UTF8 ;
+
+ select value from NLS_DATABASE_PARAMETERS where parameter=’NLS_CHARACTERSET’;
+
+shutdown immediate;
+
+startup;
